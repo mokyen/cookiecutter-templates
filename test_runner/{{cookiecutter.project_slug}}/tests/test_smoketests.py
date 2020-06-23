5f8062b98bdb54204f3b109bd30921e9c6206c6f
David <dpatter877@gmail.com>
2020-06-23 14:47:33 -0400

    Add markers and junit.

--- test_runner/{{cookiecutter.project_slug}}/tests/test_smoketests.py
+++ test_runner/{{cookiecutter.project_slug}}/tests/test_smoketests.py
@@ -5,5 +5,10 @@ def equipment():
     # Open DUT connection
     return True
 
+@pytest.mark.smoketest
 def test_equipment_init(equipment):
+    assert equipment
+
+@pytest.mark.regression
+def test_more_complicated_thing(equipment):
     assert equipment
